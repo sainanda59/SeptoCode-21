f027b3400cbb400a00fa0c5416cee477642b3a4d
Sai Nanda <saisoumyaknanda@gmail.com>
2021-10-01 14:55:34 +0530

    SEPTOCODE D11

--- Day 11/SaiSoumyakNanda_day11.cpp
+++ Day 11/SaiSoumyakNanda_day11.cpp
@@ -5,18 +5,7 @@ int main(){
     int t;cin>>t;
     while(t--){
         int n;cin>>n;
-        int c=0;
-        while(n>1){
-        c++;
-        if(n%2==1){
-        n/=2;
-        if(n%2==1 && n!=1){
-            n++;
-        }
-        }
-        else
-        n/=2;
-        }
+        int c = log2(n);
         cout<<c<<endl;
     }
 }
